dd7b479d371df77ed9dce0bfe53ed60847613165
Agarwal Jsh Kapil <133392201+Jsh-Agarwal@users.noreply.github.com>
2023-12-02 21:46:09 +0530

    Revert "build fixes"

--- techrace_app/lib/utils/ticket.dart
+++ techrace_app/lib/utils/ticket.dart
@@ -894,15 +894,13 @@ class QRcode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-    // QrImage(
-    //   // backgroundColor: Colors.white.withOpacity(0.5),
-    //   errorCorrectionLevel: QrErrorCorrectLevel.M,
-    //   data: teamId,
-    //   version: QrVersions.auto,
-    //   // size: 200.0,
-    // );
-    QrImageView(data: teamId, size: 200);
+    return QrImage(
+      // backgroundColor: Colors.white.withOpacity(0.5),
+      errorCorrectionLevel: QrErrorCorrectLevel.M,
+      data: teamId,
+      version: QrVersions.auto,
+      // size: 200.0,
+    );
   }
 }
 
